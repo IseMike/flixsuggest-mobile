70c174e60b30d6aa5594ca0d1bcd759ead23ef7b
Isaac Shepherd <shepherdi1998@gmail.com>
2023-07-17 10:07:33 -0400

    Removed Test Screen and changed function name

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,36 +1,37 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, TouchableOpacity, Text } from 'react-native';
-import { Linking } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react'
+import { View, TextInput, Button, TouchableOpacity, Text } from 'react-native'
+import { Linking } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const LoginScreen = ({ navigation }) => {
-      const [username, setUsername] = useState('');
-      const [password, setPassword] = useState('');
+      const [username, setUsername] = useState('')
+      const [password, setPassword] = useState('')
 
       const handleLogin = () => {
             // Perform login logic here
-            console.log('Login Username:', username);
-            console.log('Login Password:', password);
-      };
+            console.log('Login Username:', username)
+            console.log('Login Password:', password)
+      }
 
       const handleRegister = () => {
             // Navigate to RegisterScreen
-            navigation.navigate('Register');
-      };
+            navigation.navigate('Register')
+      }
 
-      const handleOMDbLinkPress = () => {
-            Linking.openURL('https://rapidapi.com/SAdrian/api/moviesdatabase/details');
-      };
+      const handleMDbLinkPress = () => {
+            // Open link to the api website
+            Linking.openURL('https://rapidapi.com/SAdrian/api/moviesdatabase/details')
+      }
 
       const removeLocalData = async () => {
             try {
                   // Clear all local data
-                  await AsyncStorage.clear();
-                  console.log('Local data removed successfully');
+                  await AsyncStorage.clear()
+                  console.log('Local data removed successfully')
             } catch (error) {
-                  console.error('Error removing local data:', error);
+                  console.error('Error removing local data:', error)
             }
-      };
+      }
 
       return (
             <View>
@@ -47,19 +48,21 @@ const LoginScreen = ({ navigation }) => {
                   />
                   <Button title="Login" onPress={handleLogin} />
                   <Button title="Register" onPress={handleRegister} />
+
+                  {/*This is a development button that will remove the local data in the async storage.*/}
                   <Button title="Remove Local Data" onPress={removeLocalData} />
 
 
                   <Text style={{ alignSelf: 'center', marginTop: 16, color: 'black' }}>
                         Created by Isaac Shepherd
                   </Text>
-                  <TouchableOpacity onPress={handleOMDbLinkPress}>
+                  <TouchableOpacity onPress={handleMDbLinkPress}>
                         <Text style={{ alignSelf: 'center', marginTop: 16, color: 'blue' }}>
                               Powered by MoviesDatabase
                         </Text>
                   </TouchableOpacity>
             </View>
-      );
-};
+      )
+}
 
-export default LoginScreen;
+export default LoginScreen
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,15 +1,15 @@
-import LoginScreen from './LoginScreen';
-import RegisterScreen from './RegisterScreen';
-import RegLikeGenreScreen from './RegLikeGenreScreen';
-import RegDislikeGenreScreen from './RegDislikeGenreScreen';
+import LoginScreen from './LoginScreen'
+import RegisterScreen from './RegisterScreen'
+import RegLikeGenreScreen from './RegLikeGenreScreen'
+import RegDislikeGenreScreen from './RegDislikeGenreScreen'
 import RegRateMoviesScreen from './RegRateMoviesScreen'
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
-import { View } from 'react-native';
-import Constants from 'expo-constants';
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createStackNavigator } from '@react-navigation/stack'
+import { View } from 'react-native'
+import Constants from 'expo-constants'
 
-const Drawer = createDrawerNavigator();
-const RegistrationStack = createStackNavigator();
+const Drawer = createDrawerNavigator()
+const RegistrationStack = createStackNavigator()
 
 const RegistrationStackScreen = () => (
       <RegistrationStack.Navigator initialRouteName="RegisterScreen" screenOptions={{ headerShown: false }}>
@@ -18,7 +18,7 @@ const RegistrationStackScreen = () => (
             <RegistrationStack.Screen name="RegDislikeGenre" component={RegDislikeGenreScreen} />
             <RegistrationStack.Screen name="RegRateMovie" component={RegRateMoviesScreen} />
       </RegistrationStack.Navigator>
-);
+)
 
 
 const Main = () => {
@@ -33,10 +33,9 @@ const Main = () => {
                   <Drawer.Navigator initialRouteName='Home'>
                         <Drawer.Screen name='Login' component={LoginScreen} />
                         <Drawer.Screen name='Register' component={RegistrationStackScreen} />
-                        <Drawer.Screen name='Test' component={RegRateMoviesScreen} />
                   </Drawer.Navigator>
             </View>
-      );
-};
+      )
+}
 
-export default Main;
+export default Main
